7ba07f349faa4feb01ce8999b784c3ae4583c19d
Nacho <ignacioabadie@live.com.ar>
2018-11-21 20:27:41 -0300

    Corregidos los punteros en los objetos de los test

--- test/BalizaTest.cpp
+++ test/BalizaTest.cpp
@@ -9,36 +9,39 @@
 #include "MockSensor.cpp"
 #include "MockWifiConnector.cpp"
 
+using ::testing::AtLeast;
+using ::testing::Return;
+
 // get status test from sensor class mocking http response
 TEST(Test, getBuildStatusResultIsOKAndBrainDoLedManagerTurnGreenStateOn)
 {
 	// Instancing
-	MockHttpClient mockLEDManager = new MockLEDManager();
-	MockSensor mockSensor = new MockSensor();
-	MockWifiConnector mockWifiConnector = new MockWifiConnector();
-	Brain brain = new Brain(mockLEDManager, mockSensor, mockWifiConnector);
+	MockLEDManager* mockLEDManager = new MockLEDManager();
+	MockSensor* mockSensor = new MockSensor();
+	MockWifiConnector* mockWifiConnector = new MockWifiConnector();
+	Brain* brain = new Brain(mockLEDManager, mockSensor, mockWifiConnector);
 	char* net = "net";
 	char* pass = "pass";
 	// Mocked expected actions
-	EXPECT_CALL(mockWifiConnector, connectToWifi(net, pass))
-		.Times(1);
+	EXPECT_CALL(mockWifiConnector*, connectToWifi(net, pass))
+		.Times(1)
 		.WillOnce(Return());
 
-	EXPECT_CALL(mockSensor, setHeader())
-		.Times(1);
+	EXPECT_CALL(mockSensor*, setHeader())
+		.Times(1)
 		.WillOnce(Return());
 
-	EXPECT_CALL(mockSensor, getStatus())
+	EXPECT_CALL(mockSensor*, getStatus())
 		.Times(1)
 		.WillOnce(Return(1));
 
 
-	EXPECT_CALL(mockLEDManager, turnOnLED(1))
+	EXPECT_CALL(mockLEDManager*, turnOnLED(1))
 		.Times(1)
 		.WillOnce(Return());
 
 		// Program actions
-		brain.setup("net", "pass");
+		brain.setup(net, pass);
 		int resultStatus = brain.runProgram();
 
     // Assertion
